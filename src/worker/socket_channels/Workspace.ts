a7a988ce5c35cf2618d6c090c901f53ea3ed81b3
Joseph Landreville <landrevillejoseph@gmail.com>
2021-05-04 22:20:26 -0700

    Reduce sync memory useage.

--- src/worker/socket_channels/Workspace.ts
+++ src/worker/socket_channels/Workspace.ts
@@ -1,20 +1,22 @@
 /* eslint-disable import/prefer-default-export */
 import { basename, extname } from 'path';
 import { FilterQuery, FindOptions } from '@mikro-orm/core';
+import fg from 'fast-glob';
 import { lookup } from 'mime-types';
 import pluralize from 'pluralize';
 import log from 'electron-log';
 
 import { DB } from '../../db';
 import { Workspace, File, Folder } from '../../db/entities';
-import { chunk, glob } from '../../utils';
+import { chunk } from '../../utils';
 import { SocketRequest, SocketRequestStatus, SocketResponse } from '../../utils/websocket';
 import { EntityChannel } from './Entity';
 
-const CHUNK_SIZE = 250;
-const MATCHED_PATHS_CHUNK_SIZE    = CHUNK_SIZE;
+const                  CHUNK_SIZE = 250;
+const   PATH_DISCOVERY_CHUNK_SIZE = CHUNK_SIZE * 10;
+const    MATCHED_PATHS_CHUNK_SIZE = CHUNK_SIZE;
 const FILE_ASSOCIATION_CHUNK_SIZE = CHUNK_SIZE;
-const     NEW_PATHS_CHUNK_SIZE    = CHUNK_SIZE;
+const        NEW_PATHS_CHUNK_SIZE = CHUNK_SIZE;
 
 export class WorkspaceChannel extends EntityChannel<Workspace>
 {
@@ -79,9 +81,10 @@ export class WorkspaceChannel extends EntityChannel<Workspace>
 
   async syncFiles(request: SocketRequest<number>)
   {
-    /** Sync Process
+    /**
+     * General Process Steps:
      *
-     * a - get all files and folders on current workspace
+     * a - get all folders on current workspace
      * b = list of all files on the workspace
      * c = set of all file paths matched from workspace's folders
      *
@@ -95,10 +98,17 @@ export class WorkspaceChannel extends EntityChannel<Workspace>
      * g - associate all of e to the current workspace
      * h - add all paths in c to the workspace as new files
      *
+     * This differs in order somewhat from the actual implementation below. In order to keep performance up and memory useage down,
+     * we heavily chunk queries when we ask for things from the DB since MikroORM uses an Identity Map. Typically, the Identity Map is very useful
+     * elsewhere in the application, however here it tends to get overpopulated which significantly reduces our ability to meet the performance
+     * targets listed above.
+     *
+     * https://github.com/mikro-orm/mikro-orm/issues/732#issuecomment-671874524
      */
 
     if (!request.params) return;
     const id = request.params;
+
 
     const em = DB.getNewEM();
     if (!em)
@@ -107,60 +117,111 @@ export class WorkspaceChannel extends EntityChannel<Workspace>
       return;
     }
 
-    // a ///////////////////////////////////////////////////////////////////////////////////////////////
-    const workspace = await em.findOne(Workspace, id, ['files', 'folders']).catch(error => {
-      log.error(error);
-      this.emitFailure(request);
-    });
-
-    if (!workspace)
-    {
-      this.emitFailure(request);
-      return;
-    }
-
     // respond to inital sync request
     this.getSocket()?.emit(request.responseChannel as string, { status: SocketRequestStatus.SUCCESS } as SocketResponse<void>);
     // emit general sync update message
-    this.emitSyncUpdate(workspace.id, 'Started sync...');
+    this.emitSyncUpdate(id, 'Started sync...');
 
-    // b ///////////////////////////////////////////////////////////////////////////////////////////////
-    const existingFilesOnWorkspace = workspace.files.getItems();
-    // c ///////////////////////////////////////////////////////////////////////////////////////////////
-    this.emitSyncUpdate(workspace.id, 'Searching for files (this might take a while)...');
-    const searchPaths = workspace.folders.getItems().map(folder => folder.path);
-    const matches     = (await Promise.all(glob(searchPaths))).flat();
-    const matchSet = new Set(matches.map(entry => entry.path)); // <- c
-    // d ///////////////////////////////////////////////////////////////////////////////////////////////
-    existingFilesOnWorkspace.forEach(file => matchSet.delete(file.fullPath));
-    // e ///////////////////////////////////////////////////////////////////////////////////////////////
+    const promises: Promise<number>[] = [];
+
+    const folders = await em.find(Folder, { workspace: id });
+    console.log(folders);
+    const searchPaths = folders.map(folder => folder.path);
+    searchPaths.forEach(folder => {
+
+      const fn = async () => {
+
+        const pathStream = fg.stream('**/*', {
+          cwd: folder,
+          absolute: true,
+          onlyFiles: true,
+          unique: true,
+          dot: true,
+          suppressErrors: true,
+        });
+
+        const pathChunk: string[] = Array(PATH_DISCOVERY_CHUNK_SIZE);
+        let chunkLength = 0;
+        let totalAddedFiles = 0;
+        // eslint-disable-next-line no-restricted-syntax
+        for await (const filePath of pathStream)
+        {
+          pathChunk[chunkLength] = filePath as string;
+          chunkLength += 1;
+
+          if (chunkLength === PATH_DISCOVERY_CHUNK_SIZE)
+          {
+            totalAddedFiles += await this.syncPathChunk(id, pathChunk);
+            chunkLength = 0;
+          }
+        }
+        if (chunkLength !== 0)
+        {
+          totalAddedFiles += await this.syncPathChunk(id, pathChunk.slice(0, chunkLength));
+        }
+
+        return totalAddedFiles;
+      }
+
+      promises.push(fn());
+
+    });
+    // DONE ////////////////////////////////////////////////////////////////////////////////////////////
+
+    const newFileCount = (await Promise.all(promises)).reduce((a,c) => a + c, 0);
+    this.emitSyncUpdate(id, `Added ${pluralize('file', newFileCount, true)} to the workspace!`, true);
+  }
+
+  async syncPathChunk(workspaceID: number, pathChunk: string[])
+  {
+    const em = DB.getNewEM();
+    if (!em) return 0;
+
+    const matchSet = new Set(pathChunk); // <- c
+
+    // get all files already on the workspace //////////////////////////////////////////////////////////
+    const promises: Promise<string[]>[] = [];
+    chunk(pathChunk, MATCHED_PATHS_CHUNK_SIZE * 2).forEach(c => {
+      const fn = async () => {
+        const fromDB = await em.find(File, { fullPath: c, workspaces: { id: workspaceID } });
+        em.clear();
+        return fromDB.map(file => file.fullPath);
+      };
+      promises.push(fn());
+    });
+    const existingFilesOnWorkspace = (await Promise.all(promises)).flat();
+    // remove those from those we might need to create new file records for
+    existingFilesOnWorkspace.forEach(path => matchSet.delete(path));
+
+    // get all files that are in the DB already but not associated with the workspace //////////////////
     const pathChunks = chunk(Array.from(matchSet), MATCHED_PATHS_CHUNK_SIZE);
     const queryPromises: Promise<File[]>[] = [];
 
-    pathChunks.forEach(pathChunk => {
+    pathChunks.forEach(paths => {
       const tempEM = DB.getNewEM();
       if (!tempEM) return;
 
       const eQB = tempEM.createQueryBuilder(File, 'f')
         .select('*')
         .leftJoin('f.workspaces', 'w')
-        .where({ fullPath: pathChunk })
+        .where({ fullPath: paths })
         .groupBy('f.id')
-        .having('sum(w.id = ?) = 0 or sum(w.id = ?) is NULL', [workspace.id, workspace.id]);
+        .having('sum(w.id = ?) = 0 or sum(w.id = ?) is NULL', [workspaceID, workspaceID]);
 
       queryPromises.push(eQB.getResult());
       tempEM.clear();
     });
 
     const existingFilesNotOnWorkspace = (await Promise.all(queryPromises)).flat();
-    // log.info(existingFilesNotOnWorkspace);
+    this.emitSyncUpdate(workspaceID, `Found ${existingFilesNotOnWorkspace.length} pre-existing ${pluralize('file', existingFilesNotOnWorkspace.length)} in the DB...`);
 
-    this.emitSyncUpdate(workspace.id, `Found ${existingFilesNotOnWorkspace.length} pre-existing ${pluralize('file', existingFilesNotOnWorkspace.length)} in the DB...`);
-    // f ///////////////////////////////////////////////////////////////////////////////////////////////
+    // remove existing files from those we might need to create new records for ////////////////////////
     existingFilesNotOnWorkspace.forEach(file => matchSet.delete(file.fullPath));
-    // g ///////////////////////////////////////////////////////////////////////////////////////////////
-    await this.associateFiles(workspace.id, existingFilesNotOnWorkspace, 'existing files');
-    // h ///////////////////////////////////////////////////////////////////////////////////////////////
+
+    // associate the existing files with the current workspace /////////////////////////////////////////
+    await this.associateFiles(workspaceID, existingFilesNotOnWorkspace, 'existing files');
+
+    // create new file records /////////////////////////////////////////////////////////////////////////
     const paths = Array.from(matchSet);
     const newFiles: File[] = Array(paths.length);
     for (let i = 0; i < paths.length; i += 1)
@@ -183,17 +244,17 @@ export class WorkspaceChannel extends EntityChannel<Workspace>
         // eslint-disable-next-line no-await-in-loop
         await em?.flush();
         em?.clear();
-        this.emitSyncUpdate(workspace.id, `Importing new files to the DB... (${i + 1} of ${paths.length})`);
+        this.emitSyncUpdate(workspaceID, `Importing new files to the DB... (${i + 1} of ${paths.length})`);
       }
     }
     await em?.flush();
     em?.clear();
 
-    await this.associateFiles(workspace.id, newFiles, 'new files');
-    // DONE ////////////////////////////////////////////////////////////////////////////////////////////
+    // associate the new records with the current workspace
+    await this.associateFiles(workspaceID, newFiles, 'new files');
 
-    const newFileCount = existingFilesNotOnWorkspace.length + paths.length;
-    this.emitSyncUpdate(workspace.id, `Added ${pluralize('file', newFileCount, true)} to the workspace!`, true);
+    // return the total number of files we associated to the current workspace
+    return existingFilesNotOnWorkspace.length + newFiles.length;
   }
 
   async associateFiles(workspaceID: number, files: File[], filesDescription = 'files')
