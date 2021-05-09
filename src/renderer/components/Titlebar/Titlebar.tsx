a2fb84b1c14c343155800ea71324ef7babdd825b
Joseph Landreville <landrevillejoseph@gmail.com>
2021-05-09 06:18:54 -0700

    Tweak titlebar buttons a bit.

--- src/renderer/components/Titlebar/Titlebar.tsx
+++ src/renderer/components/Titlebar/Titlebar.tsx
@@ -26,11 +26,11 @@ export default function Titlebar({ title, className }: TitlebarProps)
         {title}
       </div>
       <div className='flex-grow'/>
-      <div className='flex-none h-full no-drag flex flex-row w-28'>
-        <button type='button' onClick={() => handleWindowAction('minimize')} className='flex-grow h-full hover:bg-white hover:bg-opacity-10 focus:ring-0'>
+      <div className='flex-none h-full no-drag flex flex-row w-32'>
+        <button type='button' onClick={() => handleWindowAction('minimize')} className='flex-grow h-full hover:bg-white hover:bg-opacity-20 focus:ring-0'>
           <FontAwesomeIcon icon={faWindowMinimize}/>
         </button>
-        <button type='button' onClick={() => handleWindowAction('maximize')} className='flex-grow h-full hover:bg-white hover:bg-opacity-10 focus:ring-0'>
+        <button type='button' onClick={() => handleWindowAction('maximize')} className='flex-grow h-full hover:bg-white hover:bg-opacity-20 focus:ring-0'>
           <FontAwesomeIcon icon={faWindowMaximize}/>
         </button>
         <button type='button' onClick={() => handleWindowAction('close')} className='flex-grow h-full hover:bg-red-500 hover:bg-opacity-70 focus:ring-0 text-base'>
